31705ef60625ed1dec4f834b3f5758580344a6ec
shaifali-careeriq <shefalishfl404@gmail.com>
2026-07-04 08:24:22 +0530

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,1 @@
+import React from 'react';import ReactDOM from 'react-dom/client';import CareerIQ from './CollegeIQ.jsx';ReactDOM.createRoot(document.getElementById('root')).render(<React.StrictMode><CareerIQ/></React.StrictMode>)
